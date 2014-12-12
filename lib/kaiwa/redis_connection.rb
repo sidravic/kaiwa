efd415538f02ec4849c7f4167805215864f24cc6
Siddharth <sid.ravichandran@gmail.com>
2014-12-12 16:30:23 +0530

    Added redis connection support to kaiwa

---
+++ lib/kaiwa/redis_connection.rb
@@ -0,0 +1,36 @@
+require 'connection_pool'
+require 'redis'
+require 'uri'
+
+module Kaiwa
+	class RedisConnection
+		POOL_SIZE = 3
+		POOL_TIMEOUT = 3
+
+	#	class << self
+
+
+		def self.create(options = {})
+			ConnectionPool.new(:timeout => POOL_TIMEOUT, :size => POOL_SIZE) do 
+				build_client(options)	
+			end
+		end
+
+		private
+
+		def self.build_client(options)
+			client = Redis.new(client_options(options))
+		end
+
+		def self.client_options(options = {})
+			default_options.merge(options)
+		end
+
+		def self.default_options
+			{
+				:host => 'localhost',
+				:port => 6379
+			}
+		end
+	end
+end
